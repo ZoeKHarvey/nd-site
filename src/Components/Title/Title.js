6476ba6b87931c72939d2c09209a9b2f67d95429
Zoe <zoekharvey@gmail.com>
2020-06-18 21:24:41 -0500

    Add website project iframes

--- src/Components/Title/Title.js
+++ src/Components/Title/Title.js
@@ -8,15 +8,8 @@ function Title() {
     return(
       <section className="section--welcome-page Title">
         <div className="css-typing">
-        {/* <p>
-          FADE IN:
-        </p>
-        <p>INT. COMPUTER SCREEN - SAME</p> */}
-        {/* <p>The website loads to the welcome page for Niko Demetriou.</p>  */}
         <p>Niko Demetriou.</p>
-        <p>Screenwriter, Copywriter, and Editor.</p>
-        {/* <p className="typewriter">The visitor enters the site.</p> 
-        <p className="button__continue">(CONTINUE)</p> */}
+        <p>Writer <span>&#8226;</span> Editor</p>
         </div>
       </section>
     )
