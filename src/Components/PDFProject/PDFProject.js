4199ed076e2e5150f031c67539a1e57d752e6c3d
Zoe <zoekharvey@gmail.com>
2020-06-18 20:36:12 -0500

    Add first brochure pdf and start conditional rendering when icon clicked

--- src/Components/PDFProject/PDFProject.js
+++ src/Components/PDFProject/PDFProject.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Brochure1 from '../../assets/ParentGuide_Final1.pdf'
+import Brochure1Preview from '../../assets/Brochure1-preview.png'
 
 
 function PDFProject() {
-
+  const [pdfOn, setView] = useState(false);
+console.log(pdfOn)
 
   return(
     <div className="PDFProject">
       <h1>PDF PROJECT</h1>
+      <img src={Brochure1Preview} alt="Preview of football brochure" onClick={() => setView(true)} />
+
+     {pdfOn &&
+     <div onClick={() => setView(false)}>
+     <embed 
+       className="brochure1-pdf" 
+       src={Brochure1} 
+       type="application/pdf" 
+       width="50%" 
+       height="500px" 
+       
+     />
+     </div>
+     }
     </div>
   )
 }
