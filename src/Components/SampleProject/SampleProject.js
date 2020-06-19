afaf914f5c1ecc24cbf107ba1dc4e26e3d1766b8
Zoe <zoekharvey@gmail.com>
2020-06-18 21:35:07 -0500

    Add sample works

--- src/Components/SampleProject/SampleProject.js
+++ src/Components/SampleProject/SampleProject.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
+import sampleScreenWrite from '../../assets/sample-sw.pdf';
+import samplePreview from '../../assets/sample-preview.png';
 
 
 function SampleProject() {
-
+  const [previewOn, setPreview] = useState(false);
 
   return(
     <div className="SampleProject">
       <h1>SAMPLE PROJECT</h1>
+
+      <iframe src="https://www.baeblemusic.com/musicblog/6-16-2015/donnie-trumpet-and-the-social-experiment-surf.html" title="SOME SHIT" height="400px" width="80%"></iframe>
+      <img src={samplePreview} onClick={() => setPreview(true)} />
+      {previewOn &&
+        <div onClick={() => setPreview(false)}>
+          <embed 
+            src={sampleScreenWrite} 
+          type="application/pdf" 
+          width="50%" 
+          height="500px" />
+        </div>}
+
+  
     </div>
   )
 }
