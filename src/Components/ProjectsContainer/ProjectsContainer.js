02b2ef42a667fb28770eeacae117436353a3befa
Zoe <zoekharvey@gmail.com>
2020-06-18 21:12:55 -0500

    Add second brochure

--- src/Components/ProjectsContainer/ProjectsContainer.js
+++ src/Components/ProjectsContainer/ProjectsContainer.js
@@ -4,13 +4,16 @@ import WebProject from '../WebProject/WebProject';
 import SampleProject from '../SampleProject/SampleProject';
 import Brochure1 from '../../assets/ParentGuide_Final1.pdf'
 import Brochure1Preview from '../../assets/Brochure1-preview.png';
+import Brochure2 from '../../assets/PlayFootball_ResourceGuide.pdf';
+import Brochure2Preview from '../../assets/ Brochure2-preview.png';
+
 
 
 
 
 export const ProjectsContainer = () => {
 
-  const [pdfProjects, setpdfProjects] = useState([{ src: Brochure1, preview: Brochure1Preview }]);
+  const [pdfProjects, setpdfProjects] = useState([{ src: Brochure1, preview: Brochure1Preview }, { src: Brochure2, preview: Brochure2Preview }]);
 
   const loopProjects = pdfProjects.map(function(project, index) {
     console.log('project??? ', project)
