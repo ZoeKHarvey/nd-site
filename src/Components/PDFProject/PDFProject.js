f7b07a018879e3233e290e3b80b5b47001d7e521
Zoe <zoekharvey@gmail.com>
2020-06-27 15:02:35 -0500

    Edit popup to cover full page

--- src/Components/PDFProject/PDFProject.js
+++ src/Components/PDFProject/PDFProject.js
@@ -1,16 +1,23 @@
 import React, {useState} from 'react';
+import './PDFProject.scss';
+import PopUp from '../Popup/Popup'
 
-import './PDFProject.scss'
 
 
-function PDFProject(project) {
-  console.log('--->', project)
+
+
+function PDFProject(project, props) {
+
   const [pdfOn, setView] = useState(false);
+  const [seen, setSeen] = useState(false)
+
 
 
 
   return(
+   
     <div className="PDFProject">
+      {seen ? <PopUp /> : null}
       <img src={project.preview} alt="Preview of football brochure" onClick={() => setView(true)} />
 
      {pdfOn &&
@@ -25,6 +32,30 @@ function PDFProject(project) {
      />
      </div>
      }
+
+{/* <div className="btn" onClick={() => setSeen(true)}>
+      <button>New User?</button>
+    </div>
+
+    {seen &&
+ <div className="modal">
+     <div className="modal_content">
+   <span className="close" onClick={() => setSeen(false)}>&times;    </span>
+ <p>I'm A Pop Up!!!</p>
+</div>
+</div>
+} */}
+
+<div>
+        <div className="btn" onClick={() => setSeen(true)}>
+          <button>New User?</button>
+        </div>
+        
+      </div>
+
+
+
+
     </div>
   )
 }
